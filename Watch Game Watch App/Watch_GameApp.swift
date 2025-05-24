633a31d5a01e81e29356be54c5b1a33248901f84
llamafestt <nils.sundelin@outlook.com>
2025-05-24 08:48:08 -0700

    Initial Commit

---
+++ Watch Game Watch App/Watch_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_GameApp.swift
+//  Watch Game Watch App
+//
+//  Created by Nils Sundelin on 5/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Watch_Game_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
